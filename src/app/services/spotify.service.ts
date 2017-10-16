9de4a30fd67ca6a30891a89be76d923889b9dcc5
SiamKing <thesiameseelectric@gmail.com>
2017-10-16 14:48:55 -0700

    added html for albums and css

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,6 +10,7 @@ export class SpotifyService {
   private client_secret = spotifyKeys.client_secret;
   private token = spotifyKeys.token;
   private artistUrl: string;
+  private albumsUrl: string;
 
   constructor(private _http: Http) {
   }
@@ -27,6 +28,14 @@ export class SpotifyService {
     headers.append('Authorization', `Bearer ${this.token}`);
     this.artistUrl = `https://api.spotify.com/v1/artists/${id}`;
     return this._http.get(this.artistUrl, { headers })
+      .map(res => res.json());
+  }
+
+  getAlbums(artistId: string) {
+    let headers = new Headers();
+    headers.append('Authorization', `Bearer ${this.token}`);
+    this.albumsUrl = `https://api.spotify.com/v1/artists/${artistId}/albums`;
+    return this._http.get(this.albumsUrl, { headers })
       .map(res => res.json());
   }
 
